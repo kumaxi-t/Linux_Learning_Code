49d6b4777c6e064f87d8d2eb9714fb30e8a07afe
库玛西t <kumaxit5@gmail.com>
2026-08-14 20:52:57 +0800

    chore: 从远程仓库移除无关文件，仅保留 network_learning 和 thread_learning

--- test.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-int main(int argc, char* argv[], char* envp[]){
-  for(int i = 0; envp[i] != nullptr; i++){
-    std::cout << "envp[" << i << "]: "<< envp[i] << std::endl; 
-  }
-  return 0;
-}
